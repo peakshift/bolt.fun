c81d87756dc78dcf95297c55cd042cb88addc513
MTG2000 <mtg0987654321@gmail.com>
2022-01-26 18:36:46 +0200

    feat (hachathon): added partners cards, update header images, implement counter

--- js/hackathon.js
+++ js/hackathon.js
@@ -1,4 +1,5 @@
 
+const COUNTDOWN_DATA = "Feb 25, 2022 12:00:00"
 
 
 // Attach Collpsable lists
@@ -17,4 +18,39 @@
         window.addEventListener('resize', setCollapseMenuHeight)
     })
 
+}
+
+// Countdown timer
+{
+
+    var countDownDate = new Date(COUNTDOWN_DATA).getTime();
+    // Update the count down every 1 second
+
+    const counters = document.querySelectorAll('#register-counter .counters-list > div')
+    const daysCounter = counters[0];
+    const hrsCounter = counters[1];
+    const secsCounter = counters[2];
+    var x = setInterval(function () {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        daysCounter.textContent = days + 'd'
+        hrsCounter.textContent = hours + 'h'
+        secsCounter.textContent = seconds + 's'
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("register-counter").style.display = "none";
+        }
+    }, 1000);
 }
